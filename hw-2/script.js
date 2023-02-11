3ea526f767a725480d6302a0face9fb5c97742d7
VladislavKrs <vlad.14.vladfacel98@gmail.com>
2023-02-11 13:17:46 +0200

    Added hw-2

---
+++ hw-2/script.js
@@ -0,0 +1,7 @@
+const computingProcesses_a = prompt ("Первое число", 2);
+const computingProcesses_b = prompt ("Второе число", 2);
+
+alert (`${computingProcesses_a} + ${computingProcesses_b} = 4 
+${computingProcesses_a} - ${computingProcesses_b} = 0
+${computingProcesses_a} * ${computingProcesses_b} = 4
+${computingProcesses_a} / ${computingProcesses_b} = 1`);
