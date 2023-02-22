25adddb6427e697c9192516b5c0522f34f2c3c62
VladislavKrs <vlad.14.vladfacel98@gmail.com>
2023-02-22 17:48:15 +0200

    Added hw-8

---
+++ hw-8/scripthw-8.js
@@ -0,0 +1,19 @@
+const str = 'МЮ Арсенал МС Ліверпуль Евертон Фулхем Бренфорд Тотенхем';
+let arr = str.split (' ');{
+
+arr.sort((a, b) => {
+
+    if(a.length > b.length){
+    return 1;
+    }
+    if(a.length < b.length){
+    return -1;
+}
+    if(a.length === b.length){
+    return 0;
+}
+})
+console.log(arr);}
+
+arr.splice(1, 3);
+console.log(arr);
